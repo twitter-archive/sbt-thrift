bb71f008d374da062546ef687c1ef1bb84cd31d8
Oliver Gould <ver@twitter.com>
2011-03-13 14:03:56 -0700

    Make twisted-generation a feature of python-generation. (A flag rather than a separate class)

--- src/main/scala/com/twitter/sbt/CompileThriftPython.scala
+++ src/main/scala/com/twitter/sbt/CompileThriftPython.scala
@@ -3,7 +3,9 @@ package com.twitter.sbt
 import _root_.sbt._
 
 trait CompileThriftPython extends CompileThrift {
-  lazy val compileThriftPython = compileThriftAction("py:new_style")
+  val compileThriftPythonTwistedEnabled = false
+  val thriftSpec = if (compileThriftPythonTwistedEnabled) "py:new_style,twisted" else "py:new_style"
+  lazy val compileThriftPython = compileThriftAction(thriftSpec)
 
   lazy val autoCompileThriftPython = task {
     if (autoCompileThriftEnabled)
@@ -15,20 +17,4 @@ trait CompileThriftPython extends CompileThrift {
   }
 
   override def compileAction = super.compileAction dependsOn(autoCompileThriftPython)
-}
-
-trait CompileThriftPythonTwisted extends CompileThrift {
-  override lazy val compileThriftPythonTwisted = compileThriftAction("py:new_style,twisted")
-
-  lazy val autoCompileThriftPythonTwisted = task {
-    if (autoCompileThriftEnabled)
-      compileThriftPythonTwisted.run
-    else {
-      log.info("%s: not auto-compiling thrift-python-twisted; you may need to run compile-thrift-python-twisted manually".format(name))
-      None
-    }
-  }
-
-  override def compileAction = super.compileAction dependsOn(autoCompileThriftPythonTwisted)
-
 }
