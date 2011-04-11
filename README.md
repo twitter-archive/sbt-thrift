d4378fcc491b062db30ef0a8e7086847ef161142
Oliver Gould <ver@twitter.com>
2011-04-11 18:01:49 +0000

    1.4.0-SNAPSHOT Allow multiple target scala namespaces.

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
+Overview
+========
 
 Sbt-thrift is an sbt plugin that adds a mixin for doing thrift code auto-generation during your
 compile phase. Choose one of these three:
 
 - `CompileThriftFinagle` - create the java bindings with alternative async interfaces for finagle, in `target/gen-java/`
 - `CompileThriftJava` - create just the java bindings, in `target/gen-java/`
-- `CompileThriftPython` - create just the python bindings, in `target/gen-py/ or target/gen-py.twisted/`
+- `CompileThriftPython` - create just the python bindings, in `target/gen-py/ (or target/gen-py.twisted/)`
 - `CompileThriftRuby` - create just the ruby bindings, in `target/gen-ruby/`
 - `CompileThriftScala` - do `CompileThriftFinagle` and `CompileThriftRuby`, but also generate scala wrappers and implicit conversions in `target/gen-scala/`
+
+Scala Thrift Generation
+-----------------------
+
+`CompileThriftScala` implements a way of generating a Scala-wrapper for a
+[Finale](http://twitter.github.com/finagle) Thrift service by using JRuby to
+inspect thrift-generated ruby code.  It is therefore necessary for input thrift
+files to specify ruby and java namespace rules.  If the java namespace is not
+specified in a `CompileThriftScala` object, it is assumed to be
+<t><i>scala-namespace</i>.thrift</t>.  In order to specify these namespaces,
+project specifications must provide a list of `ThriftNamespace`s, as follows:
+
+    import sbt._
+    import com.twitter.sbt._
+    
+    /**
+     * Describes a scala project that relies on two Thrift IDLs, to be compiled to two different namespaces.
+     */
+    class TaxesProject(info: ProjectInfo)
+      extends StandardServiceProject(info)
+      with CompileThriftScala
+    {
+      def finagleVersion = "1.2.5"
+      def finagleCore = "com.twitter" % "finagle-core" % finagleVersion
+      def finagleThrift = "com.twitter" % "finagle-thrift" % finagleVersion
+      def finagleOstrich = "com.twitter" % "finagle-ostrich4" % finagleVersion
+      
+      def thriftNamespaces =
+        new ThriftNamespace("Calculator", "gov.irs.taxes.calculator") // Java namespace inferred to be gov.irs.taxes.calculator.thrift
+	:: new ThriftNamespace( "EZFile", "gov.irs.taxes.ezfile.nondefaultthriftnamespace", "gov.irs.taxes.ezfile")
+        :: Nil
+    }
+
--- src/main/scala/com/twitter/sbt/CompileThriftScala.scala
+++ src/main/scala/com/twitter/sbt/CompileThriftScala.scala
@@ -4,6 +4,11 @@ import java.io.InputStreamReader
 import _root_.sbt._
 import org.jruby.embed._
 
+
+case class ThriftNamespace(ruby: String, java: String, scala: String) {
+  def this(ruby: String, scala: String) = this(ruby, "%s.thrift".format(scala), scala)
+}
+
 /**
  * This code compiles scala wrappers for thrift. I can't figure out how to test inside this
  * package (recursive self sbt plugin tests?!), so I created a sample twitter-local project
@@ -11,15 +16,22 @@ import org.jruby.embed._
  * (with nesting and structs).  Grab it and compile it, to make sure this still works :).
  */
 trait CompileThriftScala extends DefaultProject with CompileThriftRuby with CompileThriftFinagle {
-  def scalaThriftTargetNamespace: String
+  @Deprecated
+  def scalaThriftTargetNamespace: String =
+    throw new RuntimeException("Please override def scalaThriftTargetNamespace or thriftNamespaces (latter preferred)")
 
   @Deprecated
-  def rubyThriftNamespace: String = throw new RuntimeException("Please override def rubyThriftNamespace or originalThriftNamespaces (latter preferred)")
+  def rubyThriftNamespace: String =
+    throw new RuntimeException("Please override def rubyThriftNamespace or originalThriftNamespaces (latter preferred)")
   @Deprecated
   def javaThriftNamespace = scalaThriftTargetNamespace + ".thrift"
+  @Deprecated
+  def originalThriftNamespaces = Map(rubyThriftNamespace->javaThriftNamespace)
 
   // Preferred, because it handles compiling multiple namespaces
-  def originalThriftNamespaces = Map(rubyThriftNamespace->javaThriftNamespace)
+  def thriftNamespaces: List[ThriftNamespace] = originalThriftNamespaces.map { case (r,j) =>
+    ThriftNamespace(r, j, scalaThriftTargetNamespace)
+  }.toList
 
   def idiomizeMethods = true
 
@@ -30,11 +42,11 @@ trait CompileThriftScala extends DefaultProject with CompileThriftRuby with Comp
     val container = new ScriptingContainer(LocalContextScope.SINGLETON, LocalVariableBehavior.TRANSIENT)
     container.runScriptlet(reader, "__TMP__")
     val module = container.runScriptlet("Codegen")
-    for ((_rubyThriftNamespace, _javaThriftNamespace) <- originalThriftNamespaces) {
+    thriftNamespaces.foreach { case ThriftNamespace(_rubyNs, _javaNs, _targetNs) =>
       container.callMethod(module, "run",
         (outputPath / generatedRubyDirectoryName ##).toString,
         (outputPath / generatedScalaDirectoryName ##).toString,
-        _javaThriftNamespace, _rubyThriftNamespace, scalaThriftTargetNamespace,
+        _javaNs, _rubyNs, _targetNs,
         new java.lang.Boolean(idiomizeMethods))
     }
     None
