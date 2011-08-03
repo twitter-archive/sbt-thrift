6326ff4c528751d559aea6f0f3abb3f1ce74db3a
Oliver Gould <ver@twitter.com>
2011-08-03 16:38:02 -0700

    Preserve backwards compatibility

--- src/main/scala/com/twitter/sbt/CompileThriftPython.scala
+++ src/main/scala/com/twitter/sbt/CompileThriftPython.scala
@@ -2,8 +2,16 @@ package com.twitter.sbt
 
 import _root_.sbt._
 
+/** Generate plain-old-Python bindings
+ */
 trait CompileThriftPython extends CompileThrift {
-  lazy val compileThriftPython = compileThriftAction("py:new_style")
+  @Deprecated
+  val compileThriftPythonTwistedEnabled = false
+  lazy val pythonThriftSpec = {
+    if (compileThriftPythonTwistedEnabled) "py:new_style,twisted"
+    else "py:new_style"
+  }
+  lazy val compileThriftPython = compileThriftAction(pythonThriftSpec)
 
   lazy val autoCompileThriftPython = task {
     if (autoCompileThriftEnabled) {
@@ -17,14 +25,16 @@ trait CompileThriftPython extends CompileThrift {
   override def compileAction = super.compileAction dependsOn(autoCompileThriftPython)
 }
 
+/** Generate Twisted Python bindings
+ */
 trait CompileThriftPythonTwisted extends CompileThrift {
   lazy val compileThriftPythonTwisted = compileThriftAction("py:new_style,twisted")
 
-  lazy val autoCompileThriftPython = task {
+  lazy val autoCompileThriftPythonTwisted = task {
     if (autoCompileThriftEnabled) {
-      compileThriftPython.run
+      compileThriftPythonTwisted.run
     } else {
-      log.info("%s: not auto-compiling thrift-python; you may need to run compile-thrift-python manually".format(name))
+      log.info("%s: not auto-compiling thrift-python-twisted; you may need to run compile-thrift-python-twisted manually".format(name))
       None
     }
   }
