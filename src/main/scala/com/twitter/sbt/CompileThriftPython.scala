c66567a69d0c7a11621d80f0f093d63d31602a27
Oliver Gould <ver@twitter.com>
2011-08-03 16:12:39 -0700

    Support both synchronous- and twisted-python bindings simultaneously

--- src/main/scala/com/twitter/sbt/CompileThriftPython.scala
+++ src/main/scala/com/twitter/sbt/CompileThriftPython.scala
@@ -3,12 +3,7 @@ package com.twitter.sbt
 import _root_.sbt._
 
 trait CompileThriftPython extends CompileThrift {
-  val compileThriftPythonTwistedEnabled = false
-  lazy val pythonThriftSpec = {
-    if (compileThriftPythonTwistedEnabled) "py:new_style,twisted"
-    else "py:new_style"
-  }
-  lazy val compileThriftPython = compileThriftAction(pythonThriftSpec)
+  lazy val compileThriftPython = compileThriftAction("py:new_style")
 
   lazy val autoCompileThriftPython = task {
     if (autoCompileThriftEnabled) {
@@ -20,4 +15,19 @@ trait CompileThriftPython extends CompileThrift {
   }
 
   override def compileAction = super.compileAction dependsOn(autoCompileThriftPython)
+}
+
+trait CompileThriftPythonTwisted extends CompileThrift {
+  lazy val compileThriftPythonTwisted = compileThriftAction("py:new_style,twisted")
+
+  lazy val autoCompileThriftPython = task {
+    if (autoCompileThriftEnabled) {
+      compileThriftPython.run
+    } else {
+      log.info("%s: not auto-compiling thrift-python; you may need to run compile-thrift-python manually".format(name))
+      None
+    }
+  }
+
+  override def compileAction = super.compileAction dependsOn(autoCompileThriftPythonTwisted)
 }
