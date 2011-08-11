069165746a3bc09c31bbca657f12b64d006298f1
Ryan King <ryan@twitter.com>
2011-08-11 15:28:02 -0700

    vanilla thrift

--- src/main/scala/com/twitter/sbt/CompileThrift.scala
+++ src/main/scala/com/twitter/sbt/CompileThrift.scala
@@ -8,7 +8,8 @@ import java.io.{File, FileOutputStream, BufferedOutputStream}
 
 
 object CompileThrift {
-  var cachedPath: Option[String] = None
+  var cachedFinaglePath: Option[String] = None
+  var cachedVanillaPath: Option[String] = None
 }
 
 
@@ -16,7 +17,7 @@ trait CompileThrift extends DefaultProject with GeneratedSources {
   import CompileThrift._
 
   private[this] val _thriftBinFinagle = CompileThrift.synchronized {
-    if (!cachedPath.isDefined) {
+    if (!cachedFinaglePath.isDefined) {
       // TODO: we don't discriminate between versions here (which we need to..).
       val binPath = System.getProperty("os.name") match {
         case "Mac OS X" => "thrift-finagle.osx10.6"
@@ -59,13 +60,64 @@ trait CompileThrift extends DefaultProject with GeneratedSources {
       val path = file.getAbsolutePath()
       (execTask { "chmod 0500 %s".format(path) }).run
 
-      cachedPath = Some(path)
+      cachedFinaglePath = Some(path)
     }
 
-    cachedPath.get
+    cachedFinaglePath.get
   }
 
   def thriftBinFinagle = _thriftBinFinagle
+
+  private[this] val _thriftBinVanilla = CompileThrift.synchronized {
+    if (!cachedVanillaPath.isDefined) {
+      // TODO: we don't discriminate between versions here (which we need to..).
+      val binPath = System.getProperty("os.name") match {
+        case "Mac OS X" => "thrift.osx10.6"
+        case "Linux" => System.getProperty("os.arch") match {
+          case "i386" => "thrift.linux32"
+          case "amd64" => "thrift.linux64"
+          case arch => throw new Exception(
+            "No thrift linux binary for %s, talk to william@twitter.com".format(arch))
+        }
+        case "FreeBSD" => System.getProperty("os.arch") match {
+          case arch => throw new Exception(
+            "No thrift BSD binary for %s, talk to brandon@twitter.com".format(arch))
+        }
+        case unknown => throw new Exception(
+          "No thrift binary for %s, talk to marius@twitter.com".format(unknown))
+      }
+
+      val stream = getClass.getResourceAsStream("/thrift/%s".format(binPath))
+      val file = File.createTempFile("thrift", "scala")
+      file.deleteOnExit()
+      val fos = new BufferedOutputStream(new FileOutputStream(file), 1<<20)
+      try {
+        // TODO(oliver): upgrade to 2.8 so that i can declare @scala.annotation.tailrec
+        def copy(out: java.io.OutputStream, in: java.io.InputStream) {
+          val buf = new Array[Byte](4096)
+          val len = in.read(buf)
+          if(len > 0) {
+            out.write(buf, 0, len)
+            copy(out, in)
+          }
+        }
+
+        copy(fos, stream)
+      } finally {
+        fos.close()
+      }
+
+      import Process._
+      val path = file.getAbsolutePath()
+      (execTask { "chmod 0500 %s".format(path) }).run
+
+      cachedVanillaPath = Some(path)
+    }
+
+    cachedVanillaPath.get
+  }
+
+  def thriftBinVanilla = _thriftBinFinagle
 
   def thriftSources = (mainSourcePath / "thrift" ##) ** "*.thrift"
 
